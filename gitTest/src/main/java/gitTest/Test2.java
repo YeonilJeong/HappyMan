e1a6a221723797aeb8806954b91fe8fbf6c9457d
YI <yeoniljeung@gmail.com>
2021-12-01 12:16:58 +0900

    add Test2 class

---
+++ gitTest/src/main/java/gitTest/Test2.java
@@ -0,0 +1,7 @@
+package gitTest;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("Test2 클래스를 KS 브랜치에서 작업함");
+	}
+}
--- gitTest/src/main/java/gitTest/TestMain.java
+++ gitTest/src/main/java/gitTest/TestMain.java
@@ -2,6 +2,7 @@ package gitTest;
 
 public class TestMain {
 	public static void main(String[] args) {
-		
+		System.out.println("자바 파일도 잘 형성됨");
+		System.out.println("새로운 브랜치 (yi) 에서 작업함");
 	}
 }
